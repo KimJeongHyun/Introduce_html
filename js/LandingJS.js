d75cdfcac5b6414aa9a8a42212206a7bcbd4375a
KimJeongHyun <ptr1129@hanmail.net>
2021-10-26 01:21:12 +0900

    Introduce Page 0.0.1 / Apply gsap animates

--- js/LandingJS.js
+++ js/LandingJS.js
@@ -1,18 +1,44 @@
 async function worker(){
-    const scrollController = new ScrollMagic.Controller();
-
-    const gsapScroll = gsap.to('.techStackImage',{opacity:1,duration:1})
+    const techStackScrollController = new ScrollMagic.Controller();
+    const techStackScroll = gsap.to('.techStackImage',{opacity:1,duration:1})
 
     const scrollScene = new ScrollMagic.Scene({
-        triggerElement:'.techStackImage',
+        triggerElement:'.distHR',
         duration:'50%'
-    }).setTween(gsapScroll).addTo(scrollController)
+    }).setTween(techStackScroll).addTo(techStackScrollController)
+
+    const recordScorllController = new ScrollMagic.Controller();
+
+    const recordScroll = gsap.to('.recordListContainer',{opacity:1,top:0,duration:2})
+
+    const recordScrollScene = new ScrollMagic.Scene({
+        triggerElement:'.distHR2',
+        duration:'50%'
+    }).setTween(recordScroll).addTo(recordScorllController);
 
     await gsap.to('#profileImage',{opacity:1,duration:2})
-    await gsap.to('#profileContent',{opacity:1,duration:1})
+    await gsap.to('#profileMe',{opacity:1,duration:1})
+    await gsap.to('#profileInfo',{opacity:1,duration:1})
     await gsap.to('.distHR',{opacity:1,duration:1})
     
 }
+
+const profileElem = document.getElementById('profileArea')
+const skillsElem = document.getElementById('distHR')
+const recordsElem = document.getElementById('distHR2')
+
+const moveToProfile = () =>{
+    profileElem.scrollIntoView({behavior:"smooth"});
+}
+
+const moveToSkills = () =>{
+    skillsElem.scrollIntoView({behavior:"smooth"});
+}
+
+const moveToRecords = () =>{
+    recordsElem.scrollIntoView({behavior:"smooth"});
+}
+
 
 worker();
 
