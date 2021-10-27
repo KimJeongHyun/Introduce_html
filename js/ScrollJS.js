44661c0f99719e4986d867881542ca7e11d79897
KimJeongHyun <ptr1129@hanmail.net>
2021-10-27 16:09:19 +0900

    Introduce Page 0.1.0 / TopBtn display logic changed

--- js/ScrollJS.js
+++ js/ScrollJS.js
@@ -6,7 +6,7 @@ const openFooter = document.querySelector('.openFooter')
 
 document.addEventListener('scroll',function(){
     if (window.outerHeight*0.7<window.scrollY){
-        gsap.to(topBtn,{opacity:1,display:'',duration:0.5})
+        gsap.to(topBtn,{opacity:1,display:'block',duration:0.5})
     }else{
         gsap.to(topBtn,{opacity:0,display:'none',duration:0.5})
     }
